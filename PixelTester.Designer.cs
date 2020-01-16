d6dc63a3ffd75bf6fcc8cb37735c048aeae59b92
Wraptor <wraptor@wheezie.be>
2020-01-16 20:28:30 +0100

    Initial commit

---
+++ PixelTester.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace DeadPixelTest
+{
+    partial class PixelTester
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // PixelTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "PixelTester";
+            this.Text = "Pixel tester";
+            this.Load += new System.EventHandler(this.PixelTester_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PixelTester_KeyDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PixelTester_MouseUp);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ PixelTester.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeadPixelTest
+{
+    public partial class PixelTester : Form
+    {
+        private static readonly Color[] COLORS = new Color[]
+            { Color.White, Color.Red, Color.Green, Color.Blue };
+        private int currentColor = 0;
+        private DateTime lastLeftClicked;
+        public PixelTester()
+        {
+            InitializeComponent();
+        }
+
+        private void PixelTester_Load(object sender, EventArgs e)
+        {
+            setColor(0);
+            resetWindow();
+        }
+
+        private void PixelTester_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            if (lastLeftClicked != null && lastLeftClicked > DateTime.Now.AddMilliseconds(-300))
+            {
+                lastLeftClicked = DateTime.Now;
+                Screen screen = Screen.FromControl(this);
+                switch (this.FormBorderStyle)
+                {
+                    case FormBorderStyle.Sizable:
+                        this.FormBorderStyle = FormBorderStyle.None;
+                        this.Size = screen.Bounds.Size;
+                        this.Location = screen.Bounds.Location;
+                        break;
+                    default:
+                        resetWindow();
+                        this.CenterToScreen();
+                        break;
+                }
+
+                return;
+            }
+            else
+            {
+                lastLeftClicked = DateTime.Now;
+                switchColor();
+            }
+        }
+
+        private void switchColor()
+        {
+            if (currentColor >= COLORS.Length - 1) {
+                setColor(0);
+                return;
+            }
+
+            setColor(currentColor + 1);
+        }
+        private void setColor(int color)
+        {
+            currentColor = color;
+            this.BackColor = COLORS[currentColor];
+        }
+
+        private void PixelTester_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                resetWindow();
+        }
+
+        private void resetWindow()
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Size = new Size(500, 350);
+        }
+    }
+}
